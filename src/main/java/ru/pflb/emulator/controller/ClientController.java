2dc4117191bcb22766cd4f5333edec805991f90b
piano771 <piano771@icloud.com>
2020-09-04 19:30:21 +0400

    Added search by INN, configured gitignore

--- src/main/java/ru/pflb/emulator/controller/ClientController.java
+++ src/main/java/ru/pflb/emulator/controller/ClientController.java
@@ -1,9 +1,9 @@
 package ru.pflb.emulator.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import ru.pflb.emulator.model.dto.ClientDto;
 import ru.pflb.emulator.service.ClientService;
 
@@ -17,9 +17,28 @@ public class ClientController {
     }
 
     @GetMapping("/client/{id}")
-    public ResponseEntity<ClientDto> getHello(@PathVariable long id) {
+    public ResponseEntity<ClientDto> getClient(@PathVariable long id) {
         ClientDto client = clientService.getUserById(id);
 
         return ResponseEntity.ok(client);
+    }
+
+    @PutMapping("/client/{id}")
+    public ResponseEntity<ClientDto> updateClient(@RequestBody ClientDto client) {
+        System.out.println(client);
+
+        return ResponseEntity.ok(client);
+    }
+
+    @DeleteMapping("/client/{id}")
+    public ResponseEntity<ClientDto> deleteClient(@PathVariable long id) {
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PostMapping("/client")
+    public ResponseEntity<ClientDto> addClient(@Validated @RequestBody ClientDto client) {
+
+        return ResponseEntity.status(201).build();
     }
 }
--- src/main/java/ru/pflb/emulator/service/impl/ClientServiceImpl.java
+++ src/main/java/ru/pflb/emulator/service/impl/ClientServiceImpl.java
@@ -4,12 +4,72 @@ import org.springframework.stereotype.Service;
 import ru.pflb.emulator.model.dto.ClientDto;
 import ru.pflb.emulator.service.ClientService;
 
+import java.io.File;
+import java.util.Random;
+
 @Service
 public class ClientServiceImpl implements ClientService {
 
     @Override
     public ClientDto getUserById(long id) {
-        //some code
-        return null;
+        ClientDto client = ClientDto.builder()
+                .id(id)
+                .cardNumber(generateRandomCardNumber())
+                .firstName(generateRandomName())
+                .lastName(generateRandomName())
+                .itn(generateRandomItn())
+                .img(getRandomImg())
+                .isActive(generateRandomStatus())
+                .build();
+
+        return client;
+    }
+
+    private String getRandomImg() {
+        String list[] = new File("./img").list();
+        String randomImg = list[getRandomInt(0, 2)];
+
+        return randomImg;
+    }
+
+    private String generateRandomItn() {
+        StringBuilder itn = new StringBuilder();
+        for(int i = 0; i < 10; i++) {
+            itn.append(getRandomInt(0, 9));
+        }
+
+        return itn.toString();
+    }
+
+    private boolean generateRandomStatus() {
+        if(getRandomInt(0, 10) % 2 > 0) {
+            return false;
+        } else return true;
+    }
+
+    private String generateRandomName() {
+        Random random = new Random();
+        char[] word = new char[random.nextInt(8) + 3];
+        for(int j = 0; j < word.length; j++) {
+            word[j] = (char) ('a' + random.nextInt(26));
+        }
+        String newName = new String(word);
+
+        return newName;
+    }
+
+    private String generateRandomCardNumber() {
+        StringBuilder cardNumber = new StringBuilder();
+        for(int i = 0; i < 16; i++) {
+            cardNumber.append(getRandomInt(0, 9));
+        }
+
+        return cardNumber.toString();
+    }
+
+    private int getRandomInt(int min, int max) {
+        int randomInt = (int) ((Math.random() * ((max - min) + 1)) + min);
+
+        return randomInt;
     }
 }
